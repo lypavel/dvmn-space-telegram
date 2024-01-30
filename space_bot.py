e1d2dc8d14e05d2e13dd4644f257f22932054754
lypavel <157053921+lypavel@users.noreply.github.com>
2024-01-30 12:37:47 +0300

    publish photo to telegram channel

--- space_bot.py
+++ space_bot.py
@@ -10,6 +10,11 @@ def main(chat_id: str) -> None:
 
     bot.send_message(text="Hello", chat_id=chat_id)
 
+    with open("images/nasa_apod_0.jpg", "rb") as stream:
+        data = stream.read()
+
+    bot.send_photo(photo=data, chat_id=chat_id)
+
 
 if __name__ == "__main__":
     main("@lypavel_dvmn_space")
